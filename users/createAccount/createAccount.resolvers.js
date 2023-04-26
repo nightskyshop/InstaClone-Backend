590bec018baafbf40ccf6ef89d15cb71e84d1ba3
nightskyshop1023 <nightskyshop1023@naver.com>
2023-04-26 13:46:41 +0900

    4.3 Refactor Files

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client";
+import client from "../../client";
 
 export default {
   Mutation: {
@@ -35,30 +34,6 @@ export default {
         })
       } catch (error) {
         return error;
-      }
-    },
-    login: async(_, {username, password}) => {
-      const user = await client.user.findFirst({ where: { username } })
-
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found."
-        }
-      }
-
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if(!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password."
-        }
-      }
-      
-      const token = await jwt.sign({ id: user.id }, process.env.SECERET_KEY);
-      return {
-        ok: true,
-        token
       }
     }
   }
